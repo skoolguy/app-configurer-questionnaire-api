ad92258070dfb6c6055d6151852544588640c31d
skoolguy <greatguyajmal@gmail.com>
2018-10-06 21:28:11 +0530

    registered the testRoute

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-var express = require('express'),
-  app = express(),
-  port = process.env.PORT || 3000;
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+var testRoutes = require('./api/routes/testRoute');
+
+testRoutes(app);//register the testRoute
 
 app.listen(port);
 
