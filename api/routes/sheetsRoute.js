ce98b61a86ed82dc3e4e0feb4b8655c4df737578
skoolguy <greatguyajmal@gmail.com>
2018-10-08 07:12:23 +0530

    ->deleted sheetsRoute and sheetsController ->added multer and wrote a post request for file upload in app.js

--- api/routes/sheetsRoute.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(app){
-    var sheetsController = require('../controllers/sheet-xlsx-controller');
-
-    app.route('/sheets/readFile')
-        .post(sheetsController.readFile);
-};
--- app.js
+++ app.js
@@ -2,10 +2,35 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 var testRoutes = require('./api/routes/testRoute');
-var sheetsRoute = require('./api/routes/sheetsRoute');
+var multer  = require('multer'); //For gettin multipart form data
+var path = require('path'); //used in storage engine to get file extension 
+
+//Set storage engine for multer
+var storage = multer.diskStorage({
+    destination: './public/uploadedFiles',
+    filename: function(req, file, callback){
+        callback(null, file.fieldname + Date.now() + path.extname(file.originalname));
+    }
+});
+
+//Initialize upload variable for multer - passing in storageEngine
+const upload = multer({
+    storage: storage
+}).array('excelFile');
+
+//
+app.post('/sheets/readFile', (req, res) => {
+    upload(req, res, (err) => {
+        if (err){
+            res.send(err);
+        } else {
+            console.log(req.file);
+            res.send('test');
+        }
+    });
+} );
 
 testRoutes(app);//register the testRoute
-sheetsRoute(app);//register sheetsRoute
 
 app.listen(
     port, 
