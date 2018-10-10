905ed28a0ebcb4f75c30121af4c4cf8548bd9342
skoolguy <greatguyajmal@gmail.com>
2018-10-10 07:31:26 +0530

    Got all the filenames from the public/uploadedFiles directory(prints it out). Opens up the last file in the directory as a workbook.

--- api/controllers/readExcelController.js
+++ api/controllers/readExcelController.js
@@ -1,9 +1,12 @@
 //requiring path and fs modules
 const path = require('path');
 const fs = require('fs');
+const XLSX = require('xlsx');
 
 //setting the directory path
-const directoryPath = path.join('../../public/uploadedFiles');
+const directoryPath = path.join(__dirname,'../../public/uploadedFiles');
+
+var filename, filecount;
 
 //reading the directory
 fs.readdir(directoryPath, (err, files) => {
@@ -11,9 +14,20 @@ fs.readdir(directoryPath, (err, files) => {
     if(err){
         return console.log('unable to scan directory - ' + err);
     } else {
+        filecount = files.length;
         //listing all files in the directory
         files.forEach((file) => {
             console.log('File present in the directory : '+ file);
+            filename = file;
         });
+        if (filecount > 0) {
+            readFile(directoryPath, filename);
+        }
     }
 });
+
+readFile = function(directoryPath, filename){
+    console.log('Opening file ' + filename);
+    var workbook = XLSX.readFile(path.join(directoryPath, filename));
+    console.log(workbook);
+}
